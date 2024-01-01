dae29ef34cd74af2cd6f424abb743e710f07cfa1
Jason Riley <jrileyproduction@gmail.com>
2024-01-01 04:10:19 -0500

    First submission for Assignment1

---
+++ assignment1(final)/assignment1/app.js
@@ -0,0 +1,45 @@
+(function () {
+  "use strict";
+
+  angular.module("LunchCheck", []).controller("LunchCheckController", LunchCheckController);
+
+  LunchCheckController.$inject = ["$scope"];
+  function LunchCheckController($scope) {
+    $scope.listItems = "";
+    $scope.output = "";
+    $scope.msgColor = "black";
+    $scope.textBoxColor = "";
+
+    $scope.checkList = function () {
+      var itemsArray = countItems($scope.listItems)
+      // console.log(itemsArray);
+      if (itemsArray.length <= 3 && itemsArray.length > 0) {
+        $scope.output = "Enjoy!";
+        $scope.msgColor = "green";
+        $scope.textBoxColor = "green";
+      }
+      else if (itemsArray.length > 3) {
+        $scope.output = "Too much!";
+        $scope.msgColor = "green";
+        $scope.textBoxColor = "green";
+      }
+      else if (itemsArray.length == 0) {
+        $scope.output = "Please enter data first.";
+        $scope.msgColor = "red";
+        $scope.textBoxColor = "red";
+      }
+    }
+
+    function countItems(list) {
+      var items = list.split(", ")
+      var modItems = [];
+      for (var i = 0;i < items.length;i++) {
+        if (!/[A-Za-z]/.test(items[i])) {
+          items.splice(i);
+        }
+      }
+      return items;
+    }
+  }
+
+})();
